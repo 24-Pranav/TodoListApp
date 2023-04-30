7af409dd4d2e13aa87771df1bdeee764a8914ea0
24-Pranav <rajputpranav996@gmail.com>
2023-04-30 11:38:27 +0530

    ok

--- app/src/main/java/com/batch2/todolistapp/MainActivity.java
+++ app/src/main/java/com/batch2/todolistapp/MainActivity.java
@@ -1,20 +1,25 @@
 package com.batch2.todolistapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.app.FragmentTransaction;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.*;
 import android.widget.*;
+import androidx.appcompat.widget.Toolbar;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
 {
+    ActionBar actionBar;
     DBHelper db;
     FragmentManager fm;
     FragmentTransaction ft;
@@ -22,6 +27,7 @@ public class MainActivity extends AppCompatActivity
     public ArrayList<String> tasks;
     public ArrayAdapter<String> adapter;
 
+//    private LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +36,49 @@ public class MainActivity extends AppCompatActivity
         getSupportActionBar().hide();
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
         loadFragment(new TopBarFragment());
+
+//        layout=findViewById(R.id.linearLayout);
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.actionbarr_main, (Menu) layout);
+
+
+//        LinearLayout layout = new LinearLayout(this);
+//        layout.setOrientation(LinearLayout.HORIZONTAL);
+//        layout.setLayoutParams(new LinearLayout.LayoutParams(
+//                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+//
+//        ImageView imageView=new ImageView(this);
+//        imageView.setImageResource(R.drawable.ic_launcher);
+//        imageView.setLayoutParams(new LinearLayout.LayoutParams(
+//                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+//
+//        Spinner spinner=new Spinner(this);
+//        spinner.setLayoutParams(new LinearLayout.LayoutParams(
+//                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+//
+//        layout.addView(imageView);
+//        layout.addView(spinner);
+
+//        actionBar=getSupportActionBar();
+//        if(actionBar!=null){
+//            actionBar.setDisplayShowTitleEnabled(false);
+//            actionBar.setCustomView(layout);
+//        }
+//        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.ActionbarColor)));
+//
+//        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+//                LinearLayout.LayoutParams.WRAP_CONTENT,
+//                LinearLayout.LayoutParams.WRAP_CONTENT,
+//                Gravity.LEFT);
+//
+//            imageView.setLayoutParams(params);
+//            spinner.setLayoutParams(params);
+
+
+//        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+//        loadFragment(new TopBarFragment());
 
         db = new DBHelper(this);
         tasks = db.fetchAll();
@@ -39,6 +87,9 @@ public class MainActivity extends AppCompatActivity
 
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(adapter);
+
+
+
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -57,7 +108,22 @@ public class MainActivity extends AppCompatActivity
             }
         });
     }
-
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.actionbarr_main, menu);
+//        return true;
+//    }
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.action_search:
+//                // Do something when menu_item1 is selected
+//                return true;
+////            case R.id.menu_item2:
+//                // Do something when menu_item2 is selected
+////                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
     public void addTask(View view)
     {
         EditText editText = (EditText) findViewById(R.id.editText);
--- app/src/main/java/com/batch2/todolistapp/TopBarFragment.java
+++ app/src/main/java/com/batch2/todolistapp/TopBarFragment.java
@@ -7,12 +7,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.SearchView;
 import android.widget.Spinner;
 
 
 public class TopBarFragment extends Fragment {
 Spinner s;
-String val[]={"All Lists","Default","Personal","Shopping","Wishlists","Work","Finished"};
+
     public TopBarFragment() {
         // Required empty public constructor
     }
@@ -21,13 +24,32 @@ String val[]={"All Lists","Default","Personal","Shopping","Wishlists","Work","Fi
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-//        View v=inflater.inflate(R.layout.fragment_top_bar,container,false);
-//        s=(Spinner) v.findViewById(R.id.spinner);
-//        ArrayAdapter<String>adapter=new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item,val);
-//        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-//        s.setAdapter(adapter);
+        View v=inflater.inflate(R.layout.fragment_top_bar,container,false);
+        s=(Spinner) v.findViewById(R.id.spinner);
+        ArrayAdapter<String>adapter=new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item,new String[]{"All Lists","Default","Personal","Shopping","Wishlists","Work","Finished"});
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
 
+        SearchView search = v.findViewById(R.id.search_view);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popupMenu = new PopupMenu(getContext(), view);
+                popupMenu.inflate(R.menu.actionbarr_main);
+                popupMenu.show();
+            }
+        });
+
+        ImageView more = v.findViewById(R.id.more);
+        more.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popupMenu = new PopupMenu(getContext(), view);
+                popupMenu.inflate(R.menu.more_menu);
+                popupMenu.show();
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_top_bar, container, false);
+        return v;
     }
 }
